a5cb3aebe647f72a4ed3c5dd58666ce8bfcc2e9e
erias04 <github.eliascerne@icloud.com>
2022-05-19 22:18:37 +0200

    feat: added init template

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,9 @@
-import Head from 'next/head'
-
-export async function getStaticProps() {
-  // fetch list of posts
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_page=1'
-  )
-  const postList = await response.json()
-  return {
-    props: {
-      postList,
-    },
-  }
-}
+import Head from "next/head";
 
 export default function IndexPage({ postList }) {
   return (
     <main>
-      <Head>
-        <title>Home page</title>
-      </Head>
-
-      <h1>List of posts</h1>
-
-      <section>
-        {postList.map((post) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
+      <h1>Working</h1>
     </main>
-  )
+  );
 }
